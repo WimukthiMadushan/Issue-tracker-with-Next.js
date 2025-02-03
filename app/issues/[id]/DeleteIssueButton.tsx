744741167d31dbd5483ceaa3867c14ecd6f47894
WimukthiMdaushan <bandarawgwm.21@uom.lk>
2025-02-03 07:57:33 +0530

    improve user experiance

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Spinner from '@/app/components/Spinner'
 import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
@@ -7,13 +8,16 @@ import { useState } from 'react'
 const DeleteIssueButton = ({issueId}: {issueId: number}) => {
   const router = useRouter()
   const [error, setError] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
   const onDelete = async () => {
-            try {
-              await axios.delete('/api/issues/' + issueId)
-              router.push('/issues')
-              router.refresh()
-            } catch (error) {
-              setError(true)
+    setIsDeleting(true)
+    try {
+      await axios.delete('/api/issues/' + issueId)
+      router.push('/issues')
+      router.refresh()
+      } catch (error) {
+        setIsDeleting(true);
+        setError(true)
     }
   }
   return (
@@ -28,8 +32,8 @@ const DeleteIssueButton = ({issueId}: {issueId: number}) => {
         <AlertDialog.Description>Are You sure you want to delete this issue? This action cannot be undone</AlertDialog.Description>
         <Flex mt='4' gap='3'>
           <AlertDialog.Cancel><Button variant='soft' color='gray'>Cancel</Button></AlertDialog.Cancel>
-          <AlertDialog.Action><Button color='red' onClick={onDelete}>
-            Delete Issue
+          <AlertDialog.Action><Button color='red' onClick={onDelete} disabled={isDeleting}>
+            Delete Issue {isDeleting && <Spinner />}
           </Button>
           </AlertDialog.Action>
         </Flex>
