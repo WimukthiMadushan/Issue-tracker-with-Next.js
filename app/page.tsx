e144116c614cbbf66bcac3a8f2356f7bd386ce16
WimukthiMdaushan <bandarawgwm.21@uom.lk>
2025-02-04 18:07:43 +0530

    layout home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import {prisma} from '@/prisma/client';
-import IssueChart from './IssueChart';
 import IssueSummary from './IssueSummary';
+import LatestIssues from './LatestIssues';
+import IssueChart from './IssueChart';
+import { Flex, Grid } from '@radix-ui/themes';
 
 export default async function Home() {
   const open = await prisma.issue.count({
@@ -13,5 +15,21 @@ export default async function Home() {
     where: { status: 'DONE' },
   });
 
-  return <IssueSummary open={open} inProgress={inProgress} done={done} />;
+  return (
+    <Grid columns={{ initial: '1', md: '2' }} gap="5">
+      <Flex direction="column" gap="5">
+        <IssueSummary
+          open={open}
+          inProgress={inProgress}
+          done={done}
+        />
+        <IssueChart
+          open={open}
+          inProgress={inProgress}
+          done={done}
+        />
+      </Flex>
+      <LatestIssues />
+    </Grid>
+  );
 }
