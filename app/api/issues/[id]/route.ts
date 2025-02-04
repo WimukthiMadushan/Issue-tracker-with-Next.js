f2220659ff11220971398ed47528256cb1120a83
WimukthiMdaushan <bandarawgwm.21@uom.lk>
2025-02-04 10:15:22 +0530

    Enhance the API to assign the issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import authOptions from "@/app/auth/authOptions";
-import { IssueSchema } from "@/app/validationSchemas";
+import { patchIssueSchema } from "@/app/validationSchemas";
 import { prisma } from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server"
@@ -10,17 +10,26 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
         return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
 
     const body = await request.json();
-    const validation = IssueSchema.safeParse(body);
+    const validation = patchIssueSchema.safeParse(body);
     if (!validation.success)
         return NextResponse.json(validation.error.errors, { status: 400 })
 
-    const issueId = Number(params.id);
-    if (isNaN(issueId)) {
-        return NextResponse.json({ error: "Invalid issue ID" }, { status: 400 });
+    const { assignedToUserId, title, description } = body;
+
+    if (assignedToUserId) {
+        const user = await prisma.user.findUnique({
+            where: { id: assignedToUserId },
+        });
+        if (!user)
+            return NextResponse.json(
+                { error: "Invalid user." },
+                { status: 400 }
+            );
     }
 
+
     const issue = await prisma.issue.findUnique({
-        where: { id: issueId },
+        where: { id: parseInt(params.id) },
     });
 
     if (!issue) {
@@ -28,13 +37,14 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
     }
 
     const updatedIssue = await prisma.issue.update({
-        where: {
-            id: parseInt(params.id)
-        },
+        where: { id: issue.id },
         data: {
-            title: body.title,
-            description: body.description
-        }
+            title,
+            description,
+            assignedToUserId: assignedToUserId,
+
+
+        },
     });
     return NextResponse.json(updatedIssue);
 }
